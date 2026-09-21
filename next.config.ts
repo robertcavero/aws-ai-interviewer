396670d2ac0a53faddede530220e88a062b6b906
RobertCavero <robertcavero404@gmail.com>
2026-09-21 00:32:03 -0300

    fix: env config for Amplify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Isso força o Next.js a injetar a variável DENTRO da pasta .next durante o build
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
 };
 
 export default nextConfig;
